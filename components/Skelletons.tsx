52ea62215e5175ac2a574597953d414977d35ae6
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2024-08-23 20:03:47 -0300

    feat: Card skelleton for movies done

---
+++ components/Skelletons.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function CardSkeleton() {
+  return (
+    <div className={`max-w-36 max-h-48 flex flex-col gap-3`}>
+      <Skeleton className="h-36 w-36 max-h-36 max-w-36" />
+      <div className={`flex flex-col gap-1 w-full items-start`}>
+        <Skeleton className="w-full h-4" />
+        <Skeleton className="w-full h-4" />
+      </div>
+    </div>
+  );
+}
