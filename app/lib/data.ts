6706d747ed5ff32e4c8b8313ec1069df269e8830
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2024-08-30 21:12:01 -0300

    refactor: replace de ifelse and large logic for choose between search and discover request

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,33 +12,24 @@ export async function getMovie(
     let data: CardMovie[] = [];
     let pages: TotalPages = { total_pages: 1 };
 
-    if (query) {
-      for (let i = 0; i < 2; i++){
-        const request = await searchInstance.get(
+    for (let i = 0; i < 2; i++) {
+      let request = await instance.get(
+        `?include_adult=${adult}&language=${language}&page=${
+          Number(page) + i
+        }&sort_by=popularity.desc&with_genres=${genre}&region=BR&with_original_language=en`
+      );
+      if (query) {
+        request = await searchInstance.get(
           `movie?query=${query}&include_adult=true&language=en-US&page=${
             Number(page) + i
           }`
         );
-        const toJson: { results: CardMovie[]; total_pages: TotalPages } =
-          await request.json();
-        const results: CardMovie[] = toJson?.results;
-        data = [...data, ...results];
-        pages = toJson?.total_pages;
       }
-    } else {
-      for (let i = 0; i < 2; i++) {
-        const request = await instance.get(
-          `?include_adult=${adult}&language=${language}&page=${
-            Number(page) + i
-          }&sort_by=popularity.desc&with_genres=${genre}&region=BR&with_original_language=en`
-        );
-        const toJson: { results: CardMovie[]; total_pages: TotalPages } =
-          await request.json();
-
-        pages = toJson?.total_pages;
-        const results: CardMovie[] = toJson?.results;
-        data = [...data, ...results];
-      }
+      const toJson: { results: CardMovie[]; total_pages: TotalPages } =
+        await request.json();
+      const results: CardMovie[] = toJson?.results;
+      data = [...data, ...results];
+      pages = toJson?.total_pages;
     }
 
     return { data, pages };
