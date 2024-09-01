e0bb0113c98560a00593270cdbac079ff75d0ce4
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2024-09-01 11:02:28 -0300

    refactor: add a onchange params for the page back to 1 while the user search for a movie

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -6,21 +6,22 @@ import { handleSelected as select } from "@/app/lib/utils";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export function Search() {
-  
-
   const router = useRouter();
   const path = usePathname();
   const searchparams = useSearchParams();
 
   const handleChange = useDebouncedCallback((term: string) => {
     select(term, searchparams, router, path, "query");
-  }, 400);
+  }, 300);
 
   return (
     <Input
       placeholder="Search for Movies"
       type="search"
-      onChange={(e) => handleChange(e.target.value)}
+      onChange={(e) => {
+        handleChange(e.target.value);
+        select("1", searchparams, router, path, "page");
+      }}
       className={`max-w-96 min-w-32`}
     ></Input>
   );
